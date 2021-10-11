738931d10905b7654a46bf480a0a5c5186c1e30b
Nistor Andrei <andreimn97@yahoo.com>
2021-10-11 22:19:02 +0300

    updated login page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,15 @@
-import { Box, TextField, Typography, Button } from '@mui/material';
+import { Box, TextField, Typography, Button, Link } from '@mui/material';
 import styles from './auth.module.css';
+import logo from '../../assets/icons/surface1.svg';
 
+function Nav() {
+  return <img src={logo} alt="logo" className={styles.logo} />;
+}
 export function Auth() {
   return (
     <section className={styles.login}>
       <div className={styles.opacity}>
+        <Nav />
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
           <Box
             sx={{
@@ -16,25 +21,40 @@ export function Auth() {
               borderRadius: '3px',
             }}
           >
-            <Typography variant="h3" component="h1">
+            <Typography variant="h3" component="h1" sx={{ fontSize: 32 }}>
               Sign in
             </Typography>
             <form>
-              <TextField margin="normal" fullWidth label="Email" id="email" sx={{ color: '#606060', backgroundColor: '#333' }} />
+              <TextField
+                margin="normal"
+                fullWidth
+                label="Email"
+                id="email"
+                sx={{ color: '#606060', backgroundColor: '#333', borderRadius: 1 }}
+                InputLabelProps={{
+                  style: { color: '#606060' },
+                }}
+              />
               <TextField
                 margin="normal"
                 fullWidth
                 label="Password"
                 id="pass"
-                sx={{ color: '#606060', backgroundColor: '#333' }}
+                sx={{ color: '#606060', backgroundColor: '#333', borderRadius: 1 }}
                 InputLabelProps={{
                   style: { color: '#606060' },
                 }}
               />
-              <Button variant="contained" fullWidth size="large" sx={{ textTransform: 'capitalize', marginTop: '3.125rem' }}>
+              <Button variant="contained" fullWidth size="large" sx={{ textTransform: 'capitalize', marginTop: '3.125rem', p: '.7rem 0', fontSize: 16 }}>
                 Sign in
               </Button>
             </form>
+            <Typography sx={{ marginTop: '2.5rem' }}>
+              New to SofaTV ?
+              <Link href="/" sx={{ marginLeft: '0.5rem' }} underline="hover">
+                Sign up now
+              </Link>
+            </Typography>
           </Box>
         </Box>
       </div>
