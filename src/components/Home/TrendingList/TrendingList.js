b1d2f109e91d243b0d1c4322793e5c0a56bf0936
Nistor Andrei <andreimn97@yahoo.com>
2021-10-20 21:26:58 +0300

    slider addded

--- src/components/Home/TrendingList/TrendingList.js
+++ src/components/Home/TrendingList/TrendingList.js
@@ -1,19 +1,12 @@
-import Glide from '@glidejs/glide';
-import '@glidejs/glide/dist/css/glide.core.min.css';
 import { useEffect, useState } from 'react';
-import { getTrending, imageUrl } from '../../api/api';
+import { getTrending } from '../../api/api';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import styles from './trendinglist.module.scss';
 
 export function TrendingList() {
-  const config = {
-    perView: 7,
-  };
-  const mainGlide = new Glide('.main__glide', config); // default options
   const [trending, setTrending] = useState(null);
-
-  useEffect(() => {
-    mainGlide?.mount();
-    return () => mainGlide.destroy();
-  }, [mainGlide]);
 
   useEffect(() => {
     async function setTrend() {
@@ -28,20 +21,16 @@ export function TrendingList() {
   }
 
   return (
-    <div className="main__glide">
-      <div className="glide__track" data-glide-el="track">
-        <ul className="glide__slides">
-          {trending.results?.map((trend) => {
-            return (
-              <li className="glide__slide">
-                <img src={`${imageUrl}${trend.poster_path}`} alt={trend.original_title} />
-                {trend.original_title}
-                {trend.vote_average}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+    <Swiper spaceBetween={5} slidesPerView={7} tag="ul">
+      {trending.results?.map((trend) => {
+        return (
+          <SwiperSlide tag="li" className={styles.item}>
+            <img src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2/${trend?.poster_path}`} alt={trend.original_title} />
+            {trend.original_title}
+            {trend.vote_average}
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 }
