b999d2c91dc06dfadc26ee588f826fa8de3c1cb7
Andrei Nistor <andreimn97@yahoo.com>
2021-11-01 14:25:31 +0200

    Search done

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,13 @@
-import { Nav } from '../Nav/Nav';
-import { FormSearch } from '../feature/FormSearch';
-import { useState } from 'react';
-import { search, imageUrl } from '../api/api';
-import styles from './search.module.scss';
+import { Nav } from "../Nav/Nav";
+import { FormSearch } from "../feature/FormSearch";
+import { useState } from "react";
+import { search, imageUrl } from "../api/api";
+import styles from "./search.module.scss";
+import placeholder from "../../assets/image/Group 3406.png";
+import star from "../../assets/icons/star-solid.svg";
 
 export function Search() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [movies, setMovies] = useState(null);
 
   async function handleChange(e) {
@@ -17,8 +19,6 @@ export function Search() {
     }
   }
 
-  console.log(movies);
-
   return (
     <>
       <Nav />
@@ -26,18 +26,26 @@ export function Search() {
         <FormSearch change={handleChange} value={input} />
         <section className={styles.results}>
           <h2>Search result for {input}</h2>
-          <ul>
+          <ul className={styles.grid}>
             {movies &&
               movies.results.map((movie) => {
-                console.log(movie?.profile_path);
                 return (
                   <li key={movie.id}>
                     {movie.poster_path ? (
-                      <img src={imageUrl('w370_and_h556_bestv2') + movie.poster_path} alt={movie.original_title} />
+                      <img src={!movie.poster_path ? placeholder : imageUrl("w370_and_h556_bestv2") + movie.poster_path} alt={movie.original_title} />
                     ) : (
-                      <img src={imageUrl('w370_and_h556_bestv2') + movie.profile_path} alt={movie.original_title} />
+                      <img
+                        src={!movie.profile_path ? placeholder : imageUrl("w370_and_h556_bestv2") + movie.profile_path}
+                        alt={movie.original_title}
+                      />
                     )}
                     <h3>{movie.original_title || movie.original_name || movie.name}</h3>
+                    {(movie.vote_average || movie.vote_average > 0) && (
+                      <span>
+                        <img src={star} alt="star" className={styles.star} />
+                        {movie.vote_average}
+                      </span>
+                    )}
                   </li>
                 );
               })}
