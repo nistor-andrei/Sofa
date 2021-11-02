49cfe298a57ee95ad061f597b20f3fb9500c3b2c
Nistor Andrei <andreimn97@yahoo.com>
2021-11-02 22:23:54 +0200

    overview update added all the movie details

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -26,21 +26,19 @@ export const imageUrl = function (dimension) {
   return `https://image.tmdb.org/t/p/${dimension}/`;
 };
 
-export async function getAllTrending(type, page, endpoint = "movie", day = "week") {
+export async function getAllTrending(type, page, endpoint = 'movie', day = 'week') {
   let res = await fetch(`https://api.themoviedb.org/3/${endpoint}/${type}/${day}?api_key=${apiKey}&page=${page}`);
   let data = await res.json();
   return data;
 }
 
-export function getPopular(type, source = "movie") {
+export function getPopular(type, source = 'movie') {
   return fetch(`https://api.themoviedb.org/3/${source}/${type}?api_key=${apiKey}&language=en-US&page=1`).then((res) => res.json());
 }
 
 export async function search(query, page) {
   try {
-    const data = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${query}&page=${page}`).then((res) =>
-      res.json()
-    );
+    const data = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${query}&page=${page}`).then((res) => res.json());
     return data;
   } catch (e) {
     return console.log(e);
@@ -49,5 +47,10 @@ export async function search(query, page) {
 
 export async function getMovie(mediaType, id) {
   const data = await fetch(`https://api.themoviedb.org/3/${mediaType}/${id}?api_key=${apiKey}&language=en-US`).then((res) => res.json());
+  return data;
+}
+
+export async function getExternalLinks(mediaType, id) {
+  const data = await fetch(`https://api.themoviedb.org/3/${mediaType}/${id}/external_ids?api_key=${apiKey}`).then((res) => res.json());
   return data;
 }
