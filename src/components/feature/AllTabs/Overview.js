5305427fb2e665839f72409531348404336d9683
Nistor Andrei <andreimn97@yahoo.com>
2021-11-02 20:38:15 +0200

    overview update

--- src/components/feature/AllTabs/Overview.js
+++ src/components/feature/AllTabs/Overview.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import { useParams } from "react-router";
-import { useEffect } from "react/cjs/react.development";
-import { getMovie } from "../../api/api";
-import styles from "../tabs.module.scss";
-import { imageUrl } from "../../api/api";
+import { useState } from 'react';
+import { useParams } from 'react-router';
+import { useEffect } from 'react/cjs/react.development';
+import { getMovie } from '../../api/api';
+import styles from '../tabs.module.scss';
+import { imageUrl } from '../../api/api';
 
 export function Overview() {
   const { id } = useParams();
@@ -11,7 +11,7 @@ export function Overview() {
 
   useEffect(() => {
     async function getData() {
-      const data = await getMovie("movie", id);
+      const data = await getMovie('movie', id);
       setOverview(data);
     }
     getData();
@@ -19,52 +19,59 @@ export function Overview() {
   if (!overview) {
     return <h2>Loading..</h2>;
   }
-  const date = overview.release_date.split("-").reverse().join(".");
+  const date = overview.release_date.split('-').reverse().join('.');
   function timeConvert(n) {
     let hours = n / 60;
     let rhours = Math.floor(hours);
     let minutes = (hours - rhours) * 60;
     let rminutes = Math.round(minutes);
-    return rhours + "h " + rminutes + "min";
+    return rhours + 'h ' + rminutes + 'min';
   }
   const budget = overview.budget.toLocaleString();
 
+  console.log(overview);
   const gen = overview.genres;
   return (
-    <article className={styles.info + " spacing"}>
+    <article className={styles.info + ' spacing'}>
       <div className={styles.infoLeft}>
-        <img src={imageUrl("w370_and_h556_bestv2") + overview.poster_path} alt={overview.original_title} />
+        <img src={imageUrl('w370_and_h556_bestv2') + overview.poster_path} alt={overview.original_title} />
       </div>
       <div className={styles.infoRight}>
         <h2>Storyline</h2>
         <p>{overview.overview}</p>
         <ul>
           <li>
-            Released
+            <p className={styles.titleDetails}> Released</p>
             <p>{date}</p>
           </li>
           <li>
-            Runtime
+            <p className={styles.titleDetails}> Runtime</p>
             <p>{timeConvert(overview.runtime)}</p>
           </li>
+          {budget > 0 && (
+            <li>
+              <p className={styles.titleDetails}> Budget</p>
+              <p>${budget}</p>
+            </li>
+          )}
           <li>
-            Budget
-            <p>${budget}</p>
-          </li>
-          <li>
-            Genre
+            <p className={styles.titleDetails}> Genre</p>
             {gen?.map((el) => {
-              return <a href="/">{el.name}</a>;
+              return (
+                <a href="/" key={el.id}>
+                  {el.name + ','}
+                </a>
+              );
             })}
           </li>
           <li>
-            Status
+            <p className={styles.titleDetails}> Status</p>
             {overview.status}
           </li>
           <li>
-            Production
+            <p className={styles.titleDetails}> Production</p>
             {overview.production_companies.map((company) => {
-              return <p>{company.name}</p>;
+              return <p key={company.id}>{company.name + ','}</p>;
             })}
           </li>
         </ul>
